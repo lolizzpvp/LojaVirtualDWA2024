5fd8c45f02b562b35460d751e33d76331a298408
Lorenzo Rainha <lorenzorgomes@gmail.com>
2024-10-02 13:44:50 -0300

    Alteração no main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from repositories.usuario_repo import UsuarioRepo
 from repositories.item_pedido_repo import ItemPedidoRepo
 from repositories.pedido_repo import PedidoRepo
 from repositories.produto_repo import ProdutoRepo
-from routes import auth_routes, main_routes, cliente_routes, admin_routes
-from util.auth_jwt import checar_autorizacao, checar_autenticacao, configurar_swagger_auth
+from routes import main_routes, cliente_routes, admin_routes
+from util.auth_jwt import checar_permissao, middleware_autenticacao
 from util.exceptions import configurar_excecoes
 
 ProdutoRepo.criar_tabela()
@@ -14,12 +15,17 @@ UsuarioRepo.criar_tabela()
 UsuarioRepo.inserir_usuarios_json("sql/usuarios.json")
 PedidoRepo.criar_tabela()
 ItemPedidoRepo.criar_tabela()
-app = FastAPI(dependencies=[Depends(checar_autorizacao)])
+app = FastAPI(dependencies=[Depends(checar_permissao)])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
-app.middleware("http")(checar_autenticacao)
+app.middleware(middleware_type="http")(middleware_autenticacao)
 configurar_excecoes(app)
 app.include_router(main_routes.router)
 app.include_router(cliente_routes.router)
 app.include_router(admin_routes.router)
-app.include_router(auth_routes.router)
-configurar_swagger_auth(app)
